5831c8ca1624537cc59df618b182cec7103da620
yuu <yuu@users.noreply.github.com>
2024-07-16 23:39:04 +0900

    fix(core): draw splash character

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,4 +1,6 @@
 use rand::prelude::*;
+use std::rc::Rc;
+use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
@@ -37,6 +39,33 @@ pub fn main_js() -> Result<(), JsValue> {
         (0, 255, 0),
         5,
     );
+
+    wasm_bindgen_futures::spawn_local(async move {
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+
+        let image = web_sys::HtmlImageElement::new().unwrap();
+        image.set_src("assets/resized/rhb/Idle (1).png");
+
+        let callback = Closure::once(move || {
+            if let Some(tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                tx.send(Ok(()));
+            };
+        });
+        let error_callback = Closure::once(move |err| {
+            if let Some(tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                tx.send(Err(err));
+            };
+        });
+
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
+        callback.forget();
+
+        success_rx.await;
+        let _ = context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+    });
 
     Ok(())
 }
