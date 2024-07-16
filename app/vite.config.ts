4b65acbfbb93fdf7225ed4c0f9ac5dbca9523da6
yuu <yuu@users.noreply.github.com>
2024-07-16 16:14:01 +0900

    fix: install bite wasm plugin and core

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,11 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
   plugins: [
+    wasm(),
     remix({
       future: {
         v3_fetcherPersist: true,
