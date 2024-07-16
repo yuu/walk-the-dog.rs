0a87dc11cec5729dd8db230a47292262dfb3935d
yuu <yuu@users.noreply.github.com>
2024-07-16 23:52:32 +0900

    fix(core): remove sierpinski

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -33,13 +33,6 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    sierpinski(
-        &context,
-        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-        (0, 255, 0),
-        5,
-    );
-
     wasm_bindgen_futures::spawn_local(async move {
         let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
         let success_tx = Rc::new(Mutex::new(Some(success_tx)));
@@ -68,69 +61,4 @@ pub fn main_js() -> Result<(), JsValue> {
     });
 
     Ok(())
-}
-
-fn sierpinski(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-    depth: u8,
-) {
-    draw_triangle(&context, points, color);
-    let depth = depth - 1;
-
-    let [top, left, right] = points;
-    if depth > 0 {
-        let mut rng = thread_rng();
-
-        let next_color = (
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-        );
-        let left_middle = midpoint(top, left);
-        let right_middle = midpoint(top, right);
-        let bottom_middle = midpoint(left, right);
-        sierpinski(
-            &context,
-            [top, left_middle, right_middle],
-            next_color,
-            depth,
-        );
-        sierpinski(
-            &context,
-            [left_middle, left, bottom_middle],
-            next_color,
-            depth,
-        );
-        sierpinski(
-            &context,
-            [right_middle, bottom_middle, right],
-            next_color,
-            depth,
-        );
-    }
-}
-
-fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
-    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
-}
-
-fn draw_triangle(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-) {
-    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
-    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
-
-    let [top, left, right] = points;
-    context.move_to(top.0, top.1);
-    context.begin_path();
-    context.line_to(left.0, left.1);
-    context.line_to(right.0, right.1);
-    context.line_to(top.0, top.1);
-    context.close_path();
-    context.stroke();
-    context.fill();
 }
